a022278e1cbfb0d9e0367755c7ef26538c6f982b
Moaney <ladonnamoney@gmail.com>
2019-08-26 22:13:06 -0400

    added new files for components, description, header, photo // made api call and imported useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+
+  const [photoOfTheDay, updatePhotoOfTheDay] = useState([]);
+
+  useEffect(() => {
+    axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .then(res => updatePhotoOfTheDay(res.data))
+        .catch(err => console.log(err));
+  }, [])
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        NASA Photo Of The Day 🚀
       </p>
     </div>
   );
