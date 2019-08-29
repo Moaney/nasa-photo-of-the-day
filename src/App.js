4cb4d0764104b132abd6c4a69487417485396149
Moaney <ladonnamoney@gmail.com>
2019-08-29 16:29:39 -0400

    imported bootstrap, reactstrap, styled components, webfont loader // added styled components for the app class // refactored the photo card and broke it down into more divs, added 4 styled components // obtained api key and added this to the get request // image is now has circular border

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import Photo from '../src/components/Photo';
-import "./App.css";
+import styled from 'styled-components';
 
 function App() {
   const [data, setData] = useState({});
   useEffect(() => {
       axios
-          .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+          .get(`https://api.nasa.gov/planetary/apod?api_key=FrWR3FRSNdFdPdKVMJa9dPxjuIZBuf1LfjSqTsZg`)
           .then(res => {
             console.log(res.data);
             setData(res.data)
           })
           .catch(err => console.log(err));
   }, [])
+
+  const Div = styled.div `
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  background-color: black;
+  color: white;
+  `
+  const H1 = styled.h1 `
+  font-family: 'Lexend Peta', sans-serif;
+  `
+ 
   return (
-    <div className="App">
-      <p>
+    <Div className="App">
+      <H1>
         NASA Photo Of The Day <span role="img" aria-label="rocket">🚀</span>
-      </p>
+      </H1>
       <div>
         <Photo title={data.title} url={data.url} explanation={data.explanation} />
       </div>
-    </div>
+    </Div>
   );
 }
 
